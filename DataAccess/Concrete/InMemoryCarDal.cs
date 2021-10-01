fe05d25ad8a0cba7ea39f9118be83c3314bab08b
egemenozyurek <ozyurekegemen95@gmail.com>
2021-10-01 18:27:52 +0300

    initial commit

---
+++ DataAccess/Concrete/InMemoryCarDal.cs
@@ -0,0 +1,74 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car
+                {
+                    Id = 1,
+                    BrandId = 1,
+                    ColorId = 1,
+                    ModelYear = "2020",
+                    DailyPrice = 1500,
+                    Description = "Volkswagen Passat"
+                },
+                new Car
+                {
+                    Id = 2,
+                    BrandId = 1,
+                    ColorId = 1,
+                    ModelYear = "2020",
+                    DailyPrice = 2000,
+                    Description = "Volvo XC-90"
+                },
+                new Car
+                {
+                    Id = 2,
+                    BrandId = 1,
+                    ColorId = 1,
+                    ModelYear = "2020",
+                    DailyPrice = 5000,
+                    Description = "BMW 5.20d"
+                }
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int carId)
+        {
+            return _cars.Where(c => c.Id == carId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.DailyPrice = car.DailyPrice;
+        }
+    }
+}
