26320849f1fbc28a30cbe08a6385652ef871eed1
egemenozyurek <ozyurekegemen95@gmail.com>
2021-10-18 12:34:49 +0300

    Added EF

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -8,8 +10,20 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
-            foreach (var car in carManager.GetAll())
+            CarManager carManager = new CarManager(new EFCarDal());
+            carManager.Add(new Car
+            {
+                BrandId = 1,
+                ColorId=2,
+                ModelYear = "2002",
+                DailyPrice=100,
+                Description = "Gümüş Grisi Peugeot"
+            });
+            foreach (var car in carManager.GetCarsByBrandId(1))
+            {
+                Console.WriteLine(car.Description);
+            }
+            foreach (var car in carManager.GetCarsByColorId(1))
             {
                 Console.WriteLine(car.Description);
             }
